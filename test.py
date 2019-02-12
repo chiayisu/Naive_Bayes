26c371280bb555a5883c37ab61d93ea6635cea16
IanSu <iansu1023@gmail.com>
2019-02-12 11:33:13 +0800

    implementaion & test

---
+++ test.py
@@ -0,0 +1,11 @@
+import Classifier
+import numpy as np
+import pandas as pd
+NB = Classifier.NaiveBayes()
+train_features = np.array([[1, 2, 2, 3], [1,3,2,1], [1, 2, 2, 3]])
+label = np.array([1, 1, 0])
+NB.train(features = train_features, label = label)
+features = np.array([[1,3,2,1], [1, 0, 0, 3]])
+cat, prob = NB.predict(features = features)
+print('category = ', cat)
+print('each probability = ', prob)
